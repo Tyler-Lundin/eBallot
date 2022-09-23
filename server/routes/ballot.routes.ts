3fa83a4baff25f146ab2f7972227ba97d2c53afd
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-23 15:43:10 -0700

    Set up routing between ballot.routes

--- server/routes/ballot.routes.ts
+++ server/routes/ballot.routes.ts
@@ -1,13 +1,37 @@
-import { Router, Request, Response } from 'express'
-import asyncHandler from 'express-async-handler'
+/*
+ * [BALLOT] - /api/ballot
+ *    [GET] - /api/ballot/:id - get ballot by id
+ *    [GET] - /api/ballot/feed - get feed of ballots
+ *    [POST] - /api/ballot - create ballot
+ *    [POST] - /api/ballot/:id/comment - comment on ballot
+ *    [POST] - /api/ballot/:id/unvote - unvote on ballot
+ *    [POST] - /api/ballot/:id/vote - vote on ballot
+ *    [PUT] - /api/ballot/:id - update ballot
+ *    [DELETE] - /api/ballot/:id - delete ballot
+ *    [DELETE] - /api/ballot/:id/uncomment - uncomment on ballot
+ */
+import { Router } from 'express'
+import {
+  commentBallot,
+  createBallot,
+  deleteBallot,
+  getBallotById,
+  getBallotFeed,
+  uncommentBallot,
+  unvoteBallot,
+  updateBallot,
+  voteBallot,
+} from '../controllers/ballot.controller'
 
 const router = Router()
 
-router.get(
-  '/feed',
-  asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Route 1 Controller Works' })
-  })
-)
-
+router.get('/feed', getBallotFeed)
+router.get('/:id', getBallotById)
+router.post('/', createBallot)
+router.put('/:id', updateBallot)
+router.delete('/:id', deleteBallot)
+router.post('/:id/vote', voteBallot)
+router.post('/:id/unvote', unvoteBallot)
+router.post('/:id/comment', commentBallot)
+router.delete('/:id/uncomment', uncommentBallot)
 export default router
