d4e98ff10d3161c2d016d36a0cbf042bec0d58df
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-23 00:06:50 -0700

    Types and Verify Auth Token

--- server/middleware/verifyAuthToken.ts
+++ server/middleware/verifyAuthToken.ts
@@ -1,20 +1,27 @@
-import jwt, { Secret } from 'jsonwebtoken'
 import { NextFunction, Request, Response } from 'express'
-// import timestamp from 'time-stamp'
-import { WriteToFile } from './Logger'
+import { IErrors } from '../types'
+import jwt, { Secret } from 'jsonwebtoken'
 
-const verifyAuthToken = (req: Request, res: Response, next: NextFunction) => {
-  // const authToken: string = (req.headers.authorization || req.headers.Authorization) as string
-  const authToken: string = req.cookies.authToken
-
-  if (!authToken) return res.status(400).json({ message: 'Authorization failed, Please login' })
-  jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET as Secret, (err, { user }: any) => {
-    if (err) return res.status(403).json({ err: 'INVALID AUTH TOKEN' })
-    req.user = user
-    // console.log(timestamp('[ HH:mm:ss ]'.red), 'user authorized: ', user.email)
-    WriteToFile(`auth token verified - ${user.email}`)
-    next()
-  })
+export default function verifyAuthToken(req: Request, res: Response, next: NextFunction) {
+  const authToken = req.cookies.authToken
+  const errors: IErrors = {}
+  if (!authToken) {
+    errors.authToken = 'authToken is required'
+  }
+  jwt.verify(
+    authToken,
+    process.env.JWT_SECRET,
+    {
+      algorithms: ['HS256'],
+    },
+    (err) => {
+      if (err) {
+        errors.authToken = 'authToken is invalid'
+      }
+    }
+  )
+  if (Object.keys(errors).length > 0) {
+    res.status(400).json(errors)
+    throw new Error(JSON.stringify(errors))
+  }
 }
-
-export default verifyAuthToken
--- server/routes/router.ts
+++ server/routes/router.ts
@@ -2,11 +2,17 @@ import { Router } from 'express'
 import ballotsRoutes from './ballot.routes'
 import authRoutes from './auth.routes'
 import userRoutes from './user.routes'
+import verifyAuthToken from '../middleware/verifyAuthToken'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
-router.use('/ballots', ballotsRoutes)
-router.use('/user', userRoutes)
+router.use('/ballots', verifyAuthToken, ballotsRoutes)
+router.use('/user', verifyAuthToken, userRoutes)
+// router.use('/comments', verifyAuthToken, commentsRoutes)
+// router.use('/vote', verifyAuthToken, voteRoutes)
+// router.use('/friends', verifyAuthToken, friendRoutes)
+// router.use('/requests', verifyAuthToken, requestRoutes)
+// router.use('/messages', verifyAuthToken, messageRoutes)
 
 export default router
