09f68034e8deeb583bd7b94bef8311f6bf082473
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-23 15:51:38 -0700

    removed required on phoneNumber and profilePhotoURL to allow User Registration

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -29,14 +29,13 @@ const userSchema = new Schema<IUser>(
     },
     phoneNumber: {
       type: String,
-      required: true,
       unique: true,
       trim: true,
       minlength: 10,
+      default: '0000000000',
     },
     profilePhotoURL: {
       type: String,
-      required: true,
       default: '',
     },
     reactions: [
