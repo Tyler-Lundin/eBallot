43fe0205b4e4b1da1fe67d94716ef61e37fc776d
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-23 12:29:53 -0700

    Documentation

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -1,3 +1,67 @@
+/*
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//             // U N A U T H O R I Z E D // R O U T E S //                      //
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+* Login returns a JWT token to be used for authentication
+* Register creates a new user
+* ForgotPassword sends an email to the user with a link to reset their password
+* ResetPassword resets the user's password
+* VerifyEmail verifies the user's email
+* ResendVerificationEmail sends a new verification email to the user
+*
+* [AUTH] - /api/auth
+    * [POST] - /api/auth/login
+    * [POST] - /api/auth/register
+    * [POST] - /api/auth/forgot-password
+    * [POST] - /api/auth/reset-password
+    * [POST] - /api/auth/verify-email
+    * [POST] - /api/auth/resend-verification-email
+    * [GET] - /api/auth/verify-email/:token
+    * [GET] - /api/auth/reset-password/:token
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//             // A U T H O R I Z E D  //  R O U T E S   //                      //
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*
+* Use VerifyAuthToken middleware to protect routes
+*
+* [USER] - /api/user 
+    * [GET] - /api/user/:id - get user data
+    * [PUT] - /api/user/:id - update user 
+    * [DELETE] - /api/user/:id - delete user 
+* - - - - - - - - - - - - - - - - - - - - - - - - - 
+* [COMMENT] - /api/comment 
+    * [GET] - /api/comment/:id - get comment and replies 
+    * [PUT] - /api/comment/:id - update comment
+    * [POST] - /api/comment/:id - create comment
+    * [DELETE] - /api/comment/:id - delete comment
+* - - - - - - - - - - - - - - - - - - - - - - - - - 
+* [FRIEND] - /api/friend 
+    * [GET] - /api/friend - get all friends
+    * [GET] - /api/friend/add/:id - add friend
+    * [GET] - /api/friend/remove/:id - remove friend
+    * [GET] - /api/friend/accept/:id - accept friend request
+    * [GET] - /api/friend/decline/:id - decline friend request
+    * [GET] - /api/friend/cancel/:id - cancel friend request
+    * [GET] - /api/friend/:id - get friend by id
+* - - - - - - - - - - - - - - - - - - - - - - - - - 
+* [CHAT] - /api/chat 
+    * [GET] - /api/chat - get all chats meta data (last message, last message time, etc.)
+    * [GET] - /api/chat/:id - get chat messages
+    * [POST] - /api/chat - create chat 
+    * [POST] - /api/chat/:id - send chat message
+    * [DELETE] - /api/chat/:id - delete chat message if sender
+    * [POST] - /api/chat/:id/leave - leave chat
+    * [POST] - /api/chat/:id/join - join chat
+    * [POST] - /api/chat/:id/invite - invite user to chat
+    * [POST] - /api/chat/:id/kick - kick user from chat
+* - - - - - - - - - - - - - - - - - - - - - - - - - 
+* [MESSAGE] - /api/message 
+    * [GET] - /api/message - get all messages
+    * [GET] - /api/message/:id - get message by id
+    * [POST] - /api/message - send message
+    * [DELETE] - /api/message/:id - delete message
+*/
+
 import { Router } from 'express'
 import ballotsRoutes from './ballot.routes'
 import authRoutes from './auth.routes'
