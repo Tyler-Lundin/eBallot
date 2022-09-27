99d8fd92a27e43cfdbdde5149677319900f67b8a
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-27 13:54:14 -0700

    Added State Selectors

--- client/src/app/hooks.ts
+++ client/src/app/hooks.ts
@@ -1,6 +1,9 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import type { RootState, AppDispatch } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useNavState = () => useAppSelector((state) => state.nav)
+export const useAuthState = () => useAppSelector((state) => state.auth)
+export const useThemeState = () => useAppSelector((state) => state.theme)
