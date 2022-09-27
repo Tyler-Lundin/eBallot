1a63950252a6c6123a2895163bfe470095fa7c8c
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-27 13:55:34 -0700

    Cleaned up App.tsx with useApp hook and set up an isOnMobile check to render Mobile Nav or Desktop Nav

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,22 +1,38 @@
-import styled from 'styled-components'
-import { useRoutes } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
+import styled, { createGlobalStyle } from 'styled-components'
+import Nav from './features/nav/mobile/Nav'
+import NavDesktop from './features/nav/desktop/NavDesktop'
+import { useApp } from './useApp'
+
+const GLOBAL_STYLE = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    min-width: 100vw;
+    min-height: 100vh;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`
 
 function App() {
-  const router = useRoutes([
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/dash/',
-      element: <Dashboard />,
-    },
-  ])
-  return <APP>{router}</APP>
+  const { routes, isOnMobile } = useApp()
+  return (
+    <APP>
+      <GLOBAL_STYLE />
+      {isOnMobile && <Nav />}
+      {!isOnMobile && <NavDesktop />}
+      {routes}
+    </APP>
+  )
 }
 
 export default App
 
-const APP = styled.div``
+const APP = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(90, 90, 90);
+`
