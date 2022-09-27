819b02834309088c834212a29bf1e896947daa11
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-27 14:05:25 -0700

    Created auth.api.ts, a folder for createAsyncThunks using Axios to interact with the API

---
+++ client/src/app/api/auth.api.ts
@@ -0,0 +1,25 @@
+import { ILoginForm, IRegisterForm } from '../../types'
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { auth } from '.'
+
+const registerUser = createAsyncThunk('auth/register', async (form: IRegisterForm, thunkAPI) => {
+  try {
+    const response = await axios.post(auth.register, form)
+    console.log('response: ', response)
+    console.log('response.data: ', response.data)
+    if (response.status > 200 && response.status < 300) {
+      return response.data
+    }
+  } catch (error: any) {
+    console.log('error: ', error)
+    return thunkAPI.rejectWithValue(error.response.data.message)
+  }
+})
+
+const loginUser = createAsyncThunk('auth/loginUser', async (form: ILoginForm) => {
+  const response = await axios.post('/api/auth/login')
+  return response.data
+})
+
+export { registerUser, loginUser }
