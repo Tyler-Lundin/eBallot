11acbb4506f11209bdc83fca8dedb967824ba8d5
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 15:31:56 -0700

    Added userSlice

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import authSlice from './features/auth/authSlice'
 import navSlice from './features/nav/navSlice'
 import themeSlice from './features/theme/themeSlice'
+import userSlice from './features/user/userSlice'
 
 export const store = configureStore({
   reducer: {
     nav: navSlice,
     auth: authSlice,
     theme: themeSlice,
+    user: userSlice,
   },
 })
 
