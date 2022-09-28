afb924ff9222f695fbf841fdcafd014fce07b4d5
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 15:47:40 -0700

    Deleted over complications

--- client/src/app/features/auth/components/Button.tsx
+++ client/src/app/features/auth/components/Button.tsx
@@ -1,25 +1,14 @@
 import styled, { css } from 'styled-components'
-import Spinner from '../../../util/Spinner'
 
-const Button = ({
-  buttonClicked,
-  onClick,
-  name,
-}: {
-  buttonClicked: boolean
-  onClick: () => void
-  name: string
-}) => {
+const Button = ({ onClick, name }: { onClick: () => void; name: string }) => {
   return (
-    <BUTTON_CONTAINER $clicked={buttonClicked}>
+    <BUTTON_CONTAINER>
       <BUTTON
-        $clicked={buttonClicked}
         onClick={(e) => {
           e.preventDefault()
           onClick()
         }}>
         {name}
-        {buttonClicked && <LOADING_CONTAINER children={<Spinner />} />}
       </BUTTON>
     </BUTTON_CONTAINER>
   )
@@ -27,26 +16,29 @@ const Button = ({
 
 export default Button
 
-const BUTTON = styled.button<{ $clicked: boolean }>`
+const BUTTON = styled.button`
   width: 100%;
-  height: 100%;
-  background-color: rgb(180, 180, 180);
+  height: 80px;
   border: 1px solid black;
   border-radius: 10px;
   font-size: 1.5rem;
   font-weight: 700;
-  color: white;
+  color: black;
   position: relative;
   cursor: pointer;
   transition: all 250ms;
   padding: 20px;
   display: grid;
   place-content: center;
-  ${({ $clicked }) => $clicked && 'color: black;'}
   overflow: hidden;
+  align-self: center;
+  &:hover {
+    background-color: black;
+    color: white;
+  }
 `
 
-const BUTTON_CONTAINER = styled.section<{ $clicked: boolean }>`
+const BUTTON_CONTAINER = styled.section`
   width: 100%;
   height: 100%;
   background-color: rgb(200, 200, 200);
@@ -56,34 +48,4 @@ const BUTTON_CONTAINER = styled.section<{ $clicked: boolean }>`
   transition: 250ms;
   display: grid;
   top: 0;
-  ${({ $clicked }) =>
-    !$clicked &&
-    css`
-      :hover ${BUTTON} {
-        background-color: rgb(100, 100, 100);
-      }
-    `}
-  ${({ $clicked }) =>
-    $clicked &&
-    css`
-      transform: scale(500%);
-      top: -160%;
-    `}
-`
-
-const LOADING_CONTAINER = styled.section`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  display: grid;
-  place-content: center;
-  animation: fadeIn 250ms forwards;
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
 `
