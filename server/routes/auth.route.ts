01f1558c76f2ef471efe704070a5336b8c77781c
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 17:35:36 -0700

    Moved code around

--- server/routes/auth.route.ts
+++ server/routes/auth.route.ts
@@ -4,20 +4,27 @@
  */
 
 import { Router, Request, Response } from 'express'
+import loginFormValidation from '../middleware/loginFormValidation'
+import registerFormValidation from '../middleware/registerFormValidation'
 import {
   loginUser,
   registerUser,
   forgotPassword,
   resetPassword,
-  verifyEmail,
-  resendVerificationEmail,
-  verifyEmailToken,
-  resetPasswordToken,
 } from '../controllers/auth.controller'
-import loginFormValidation from '../middleware/loginFormValidation'
-import registerFormValidation from '../middleware/registerFormValidation'
 
 const router = Router()
+
+router.post('/login', loginFormValidation, loginUser)
+router.post('/register', registerFormValidation, registerUser)
+router.post('/forgot-password', forgotPassword)
+router.post('/reset-password', resetPassword)
+// router.post('/verify-email', verifyEmail)
+// router.post('/resend-verification-email', resendVerificationEmail)
+// router.get('/verify-email/:token', verifyEmailToken)
+// router.get('/reset-password/:token', resetPasswordToken)
+
+export default router
 
 // * [AUTH] - /api/auth
 //     * [POST] - /api/auth/login
@@ -28,14 +35,3 @@ const router = Router()
 //     * [POST] - /api/auth/resend-verification-email
 //     * [GET] - /api/auth/verify-email/:token
 //     * [GET] - /api/auth/reset-password/:token
-
-router.get('/login', loginFormValidation, loginUser)
-router.post('/register', registerFormValidation, registerUser)
-router.post('/forgot-password', forgotPassword)
-router.post('/reset-password', resetPassword)
-router.post('/verify-email', verifyEmail)
-router.post('/resend-verification-email', resendVerificationEmail)
-router.get('/verify-email/:token', verifyEmailToken)
-router.get('/reset-password/:token', resetPasswordToken)
-
-export default router
