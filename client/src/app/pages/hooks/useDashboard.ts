8e152856ae5260460757dd26543da8c860aa3d5d
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 22:05:07 -0700

    Updated from initial useDashboard

--- client/src/app/pages/hooks/useDashboard.ts
+++ client/src/app/pages/hooks/useDashboard.ts
@@ -1,16 +1,28 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useAuthState } from '../../hooks'
+import useAuthentication from './useAuthentication'
 
 const useDashboard = () => {
-  const { authenticated } = useAuthState()
+  const { authenticated } = useAuthentication() // redirect to login if not authenticated also returns authenticated boolean
   const navTo = useNavigate()
 
-  useEffect(() => {
-    if (!authenticated) {
-      navTo('/auth/login')
-    }
-  }, [])
+  const [isActive, setIsActive] = useState({
+    ANNOUNCEMENT_BANNER: true,
+    WELCOME_MESSAGE: true,
+    FRIENDS_LIST: true,
+  })
+
+  const toggleActive = (key: string) => {
+    setIsActive((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key as keyof typeof prevState],
+    }))
+  }
+
+  return {
+    isActive,
+    toggleActive,
+  }
 }
 
 export default useDashboard
