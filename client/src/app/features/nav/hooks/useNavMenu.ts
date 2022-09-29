78ac57123748e6be4c9d11cce3841f2270ba4f1e
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 22:02:36 -0700

    Links update with authentication

--- client/src/app/features/nav/hooks/useNavMenu.ts
+++ client/src/app/features/nav/hooks/useNavMenu.ts
@@ -1,25 +1,30 @@
+import { useEffect, useState } from 'react'
 import { useAppDispatch, useAuthState, useNavState } from '../../../hooks'
 import { toggleNav } from '../navSlice'
+
+const loggedOutLinks = [
+  { to: '/', label: 'Home' },
+  { to: '/about', label: 'About' },
+  { to: '/auth/login', label: 'Login' },
+  { to: '/auth/register', label: 'Register' },
+]
+
+const loggedInLinks = [
+  { to: '/dashboard', label: 'Home' },
+  { to: '/profile', label: 'My Profile' },
+  { to: '/auth/logout', label: 'Logout' },
+]
 
 const useNavMenu = () => {
   const { isOpen } = useNavState()
   const dispatch = useAppDispatch()
   const closeNav = () => dispatch(toggleNav(false))
   const { authenticated } = useAuthState()
+  const [links, setLinks] = useState(loggedOutLinks)
 
-  const loggedOutLinks = [
-    { to: '/', label: 'Home' },
-    { to: '/about', label: 'About' },
-    { to: '/auth/login', label: 'Login' },
-    { to: '/auth/register', label: 'Register' },
-  ]
-
-  const loggedInLinks = [
-    { to: '/dashboard', label: 'Home' },
-    { to: '/profile', label: 'My Profile' },
-    { to: '/auth/logout', label: 'Logout' },
-  ]
-  const links = authenticated ? loggedInLinks : loggedOutLinks
+  useEffect(() => {
+    setLinks(authenticated ? loggedInLinks : loggedOutLinks)
+  }, [authenticated])
 
   return {
     isOpen,
