d56d86ae9305823deef50f6a66cbd381d89f56f0
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 21:56:38 -0700

    created useUser, useUserState, and useTheme

--- client/src/app/hooks.ts
+++ client/src/app/hooks.ts
@@ -1,4 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { ITheme, IUser } from '../types'
 import type { RootState, AppDispatch } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
@@ -7,3 +8,6 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export const useNavState = () => useAppSelector((state) => state.nav)
 export const useAuthState = () => useAppSelector((state) => state.auth)
 export const useThemeState = () => useAppSelector((state) => state.theme)
+export const useUserState = () => useAppSelector((state) => state.user)
+export const useUser = () => useUserState().user
+export const useTheme = () => useThemeState().currentTheme as ITheme
