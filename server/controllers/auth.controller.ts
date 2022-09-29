cbd9716645a1e39661c9b296c05d7a72408299fe
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 17:28:26 -0700

    Updated registerUser controller

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -6,23 +6,33 @@ import { Request, Response } from 'express'
 import asyncHandler from 'express-async-handler'
 import User from '../models/user.model'
 import bcrypt from 'bcryptjs'
-import genAuthToken from '../util/genAuthToken'
+import createAuthToken from '../util/createAuthToken'
 
 export const loginUser = asyncHandler(async (req: Request, res: Response) => {
   const { username, password } = req.body
   const user = await User.findOne({ $or: [{ username }, { email: username }] })
   if (user && (await bcrypt.compare(password, user.password))) {
-    const authToken = genAuthToken(user)
+    const authToken = createAuthToken(user)
     res.cookie('authToken', `BEARER ${authToken}`).json({ message: 'Login successful' })
   }
+  res.status(401).json({ message: 'Invalid username or password' })
 })
 
 export const registerUser = asyncHandler(async (req: Request, res: Response) => {
   const { username, email, password } = req.body
   const userExists = await User.findOne({ $or: [{ username }, { email }] })
   if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
+    if (userExists.username === username && userExists.email === email) {
+      res.status(400).json({ message: 'Username and email already exists' })
+    }
+    if (userExists.email === email) {
+      res.status(400).json({ message: 'Email already exists' })
+      throw new Error('Email already exists')
+    }
+    if (userExists.username === username) {
+      res.status(400).json({ message: 'Username already exists' })
+      throw new Error('Username already exists')
+    }
   }
   const user = await User.create({
     username,
@@ -30,7 +40,7 @@ export const registerUser = asyncHandler(async (req: Request, res: Response) => 
     password: await bcrypt.hash(password, 10),
   })
   if (user) {
-    const authToken = genAuthToken(user)
+    const authToken = createAuthToken(user)
     res.cookie('authToken', `BEARER ${authToken}`).json({ message: 'Register successful' })
   }
   res.status(500).json({ message: 'Server error' })
@@ -74,15 +84,15 @@ export const resendVerificationEmail = asyncHandler(async (req: Request, res: Re
   res.json({ message: 'Email sent' })
 })
 
-export const verifyEmailToken = asyncHandler(async (req: Request, res: Response) => {
-  const user = await User.findOne({ _id: req.params.id })
-  if (user) {
-    user.isVerified = true
-    await user.save()
-    res.json({ message: 'Email verified' })
-  }
-  res.status(404).json({ message: 'User not found' })
-})
+// export const verifyEmailToken = asyncHandler(async (req: Request, res: Response) => {
+//   const user = await User.findOne({ _id: req.params.id })
+//   if (user) {
+//     user.isVerified = true
+//     await user.save()
+//     res.json({ message: 'Email verified' })
+//   }
+//   res.status(404).json({ message: 'User not found' })
+// })
 
 export const resetPasswordToken = asyncHandler(async (req: Request, res: Response) => {
   const user = await User.findOne({ _id: req.params.id })
