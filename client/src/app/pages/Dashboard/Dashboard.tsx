6cb899697848927ea9a73f31d45e41e5af4abefa
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 22:03:29 -0700

    Added Welcome and Friends

--- client/src/app/pages/Dashboard/Dashboard.tsx
+++ client/src/app/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import styled from 'styled-components'
+import Friends from '../../features/friend/components/Friends'
+import Welcome from '../../features/friend/components/WelcomeMessage'
 import useDashboard from '../hooks/useDashboard'
 
 const Dashboard = () => {
   useDashboard()
   return (
     <DASHBOARD>
-      <Outlet />
+      <Welcome />
+      <Friends />
+      {/* <Outlet /> */}
+      {/* <FEED OF BALLOTS /> */}
     </DASHBOARD>
   )
 }
@@ -16,5 +21,11 @@ export default Dashboard
 const DASHBOARD = styled.div<{ theme: string }>`
   width: 100vw;
   height: 100vh;
-  background-color: rgb(25, 25, 25);
+  background-color: rgb(20, 20, 20);
+  padding: 10px;
+  padding-top: 80px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `
