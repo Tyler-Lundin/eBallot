2c38e88bcc80519c267d88bde48b1b56d613a2eb
Tyler Lundin <TylerJamesLundin@gmail.com>
2022-09-28 22:04:05 -0700

    Added userStats to userSchema

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -9,6 +9,52 @@ import bcrypt from 'bcryptjs'
 
 const userSchema = new Schema<IUser>(
   {
+    userStats: {
+      ballotStats: {
+        totalVotes: {
+          type: Number,
+          default: 0,
+        },
+        totalComments: {
+          type: Number,
+          default: 0,
+        },
+        totalReactions: {
+          type: Number,
+          default: 0,
+        },
+      },
+      commentStats: {
+        totalReplies: {
+          type: Number,
+          default: 0,
+        },
+        totalReactions: {
+          type: Number,
+          default: 0,
+        },
+      },
+      reactionStats: {
+        totalReactions: {
+          type: Number,
+          default: 0,
+        },
+      },
+      friendStats: {
+        totalFriends: {
+          type: Number,
+          default: 0,
+        },
+        totalMessages: {
+          type: Number,
+          default: 0,
+        },
+      },
+      viewCount: {
+        type: Number,
+        default: 0,
+      },
+    },
     email: {
       type: String,
       required: true,
@@ -44,13 +90,13 @@ const userSchema = new Schema<IUser>(
         ref: 'Reaction',
       },
     ],
-    requestIDs: [
+    requests: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Request',
       },
     ],
-    voteIDs: [
+    votes: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Vote',
